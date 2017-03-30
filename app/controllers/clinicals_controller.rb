e15e883186d9abd0b0231ec1f75449d13328de95
thestrangeknight <rishu97kr@gmail.com>
2017-03-30 20:01:55 +0530

    Change insti. profile

--- app/controllers/clinicals_controller.rb
+++ app/controllers/clinicals_controller.rb
@@ -1,10 +1,17 @@
   class ClinicalsController < ApplicationController
   before_action :set_institution, only: [:create, :show]
-  before_action :set_clinical, only: [:update]
+  before_action :set_clinical, only: [:update, :destroy]
 
   # GET /clinicals
   def index
-    response = Clinical.uniq.pluck(:clinical_name, :institution_id)
+    tmp = Clinical.all
+    response = []
+    tmp.each do |clinical|
+      obj = {}
+      obj['speciality_name'] = clinical['speciality_name']
+      obj['clinicals'] = clinical.as_json(except: [:created_at, :updated_at, ] )
+      response << obj
+    end
     render json: response
   end
 
@@ -16,14 +23,16 @@
 
   # POST /clinicals
   def create
-    @clinical = Clinical.new(clinical_params)
-    @institution.clinicals << @clinical
-
-    if @clinical.save
-      render json: @clinical, status: :created
-    else
-      render json: @clinical.errors, status: :unprocessable_entity
+    params[:clinicals].each do |clinical|
+      @clinical = Clinical.new()
+      @clinical.speciality_name = clinical['speciality_name']
+      @clinical.doctor_name = clinical['doctor_name']
+      @clinical.department = clinical['department']
+      @institution.clinicals << @clinical
+      @clinical.save
     end
+
+    render json: @institution.clinicals
   end
 
   # PATCH/PUT /clinicals/1
@@ -37,7 +46,9 @@
 
   # DELETE /clinicals/1
   # No need for this call
-  def destroy; end
+  def destroy
+    @clinical.destroy
+  end
 
   private
 
@@ -52,6 +63,6 @@
 
   # Only allow a trusted parameter "white list" through.
   def clinical_params
-    params.fetch(:clinical, {}).permit(:speciality_name, :doctor_name, :experience, :department)
+    params.fetch(:clinical, {}).permit(:speciality_name, :doctor_name, :department, clinicals: [:speciality_name, :doctor_name, :department])
   end
 end
