e9837c88f5c106e6974af02f4469b4fea0605f07
Priya-sopho <prrani28@gmail.com>
2017-03-31 17:07:29 +0530

    Improved timeslot controller

--- app/controllers/timeslots_controller.rb
+++ app/controllers/timeslots_controller.rb
@@ -9,6 +9,10 @@ end
 #GET timeslots
 def index
   @timeslots = Timeslot.all
+  @timeslots.each do |i|
+   i.day = string_day_to_array(i.day) 
+  puts ("Hello")
+  end
   render json: @timeslots
 end
 
@@ -49,19 +53,19 @@ private
 
 
  def string_day_to_array(day)
-  day = day.split('')
+  d = day.split('')
   days = []
-  if d[0] == 1 then days.push("Monday")
+  if d[0] == "1" then days.push("Monday")
    end    
-  if d[1] == 1 then days.push("Tuesday")
+  if d[1] == "1" then days.push("Tuesday")
    end
-  if d[2] == 1 then days.push("Wednesday")
+  if d[2] == "1" then days.push("Wednesday")
    end
-  if d[3] == 1 then days.push("Thursday")
+  if d[3] == "1" then days.push("Thursday")
    end
-  if d[4] == 1 then days.push("Friday")
+  if d[4] == "1" then days.push("Friday")
    end
-  if d[5] == 1 then days.push("Saturday")
+  if d[5] == "1" then days.push("Saturday")
    end
  return days
  end
