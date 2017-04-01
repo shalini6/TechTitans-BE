f4cb05beb253a8626f2c177e546c5370a6f6fa2b
shalini6 <slini06@gmail.com>
2017-04-01 20:41:23 +0530

    Appointments

--- config/routes.rb
+++ config/routes.rb
@@ -12,11 +12,8 @@ Rails.application.routes.draw do
 
  resources :timeslots
  resources :rates
-<<<<<<< HEAD
- resources :patients
+
+ 
  resources :videos
-=======
-  
->>>>>>> d2304d56da8fce0ca16e8c77c78dde252ef6391b
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
